ab05b1928ae42f69bb7daee92c6a2a952f78c7f9
Satyaprakash302 <satya.singh_bca24@gla.ac.in>
2025-08-19 20:16:21 +0530

    First commit

---
+++ max2num.py
@@ -0,0 +1,8 @@
+A = float(input("Enter first number"))
+B = float(input("Enter second number"))
+if A > B:
+    print(" A is max", A)
+elif B > A:
+    print(" B is max", B)
+else:
+    print("Both numbers are equal.")
